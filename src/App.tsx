5e1d5d62d91e41245ed47812b3fba1bf6c79379e
sanel-pajic <sanelpa@hotmail.com>
2019-12-02 21:52:25 +0100

    fix(*): New features added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+//import { EditableTable } from "./components/EditableTable";
+import { InputBlogComponent } from "./components/InputBlogComponent";
+import { TableBlogPosts } from "./components/TableBlogPosts";
+//import { TableMUITest } from "./components/TableMUITest";
+import Example from "./components/Example"
+import Form from "./components/Form";
+
+const client = new ApolloClient({
+  uri: "http://localhost:8080/graphql"
+});
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div
+        className="App"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          margin: 0,
+          padding: 0
+        }}
+      >
+        <InputBlogComponent/>
+        
+        <TableBlogPosts/>
+     
+      </div>
+    </ApolloProvider>
   );
-}
+};
 
 export default App;
---
+++ src/components/TableData.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+
+const useStyles = makeStyles({
+  root: {
+    width: "800px",
+    overflowX: "auto",
+    margin: "50px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  table: {
+    minWidth: 650
+  }
+});
+
+function createData(name: string, calories: number, fat: number) {
+  return { name, calories, fat };
+}
+
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0),
+  createData("Ice cream sandwich", 237, 9.0),
+  createData("Eclair", 262, 16.0),
+  createData("Cupcake", 305, 3.7),
+  createData("Gingerbread", 356, 16.0)
+];
+
+export const TableData: React.FC = () => {
+  const classes = useStyles();
+  return (
+    <Paper className={classes.root}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Title</TableCell>
+            <TableCell align="right">Description</TableCell>
+            <TableCell align="right">Option Button</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map(row => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
+  );
+};
